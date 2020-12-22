9ac96235985075ac721cb91fa12d117db2709f9d
myrabokhari <69834787+myrabokhari@users.noreply.github.com>
2020-12-22 13:50:40 -0500

    making allow_major_version_upgrade true

--- main.tf
+++ main.tf
@@ -244,7 +244,7 @@ locals {
 resource "aws_db_instance" "db_instance" {
 
   allocated_storage                   = local.storage_size
-  allow_major_version_upgrade         = false
+  allow_major_version_upgrade         = true
   apply_immediately                   = var.apply_immediately
   auto_minor_version_upgrade          = var.auto_minor_version_upgrade
   backup_retention_period             = var.read_replica ? 0 : var.backup_retention_period
